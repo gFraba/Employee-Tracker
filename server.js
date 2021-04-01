2cfaaf89bce5b645c53fff3cdeaff1a16fb7f1f8
gfraba <joshua.gagle@gmail.com>
2021-04-01 19:05:29 -0400

    connection to db and menu function

--- server.js
+++ server.js
@@ -7,3 +7,70 @@ figlet("Employee Tracker", (err, data) => {
     if (err) throw err;
     console.log(data);
 });
+
+//connection to db
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "employee_management_db"
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('connection established!');
+    start();
+});
+
+// main menu
+function start() {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View all departments",
+            "View all roles",
+            "View all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update employee role",
+            "Exit"
+        ]}).then((answer) => {
+        switch (answer.action) {
+            case "View all departments":
+                viewDepts();
+                break;
+
+            case "View all roles":
+                viewRoles();
+                break;
+
+            case "View all employees":
+                viewEes();
+                break;
+
+            case "Add a department":
+                addDept();
+                break;
+
+            case "Add a role":
+                addRole();
+                break;
+
+            case "Add an employee":
+                addEe();
+                break;
+
+            case "Update employee role":
+                update();
+                break;
+
+            case "Exit":
+                connection.end();
+                break;
+        }
+    });
+};
